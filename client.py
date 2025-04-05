7709b582ba573fb9424060d35cc95c785c53fc8c
Ranim06 <ranim.benslimen@esen.tn>
2025-04-05 15:15:24 +0100

    Update client.py

--- client.py
+++ client.py
@@ -2,16 +2,17 @@ import socket
 
 # Connect to the server
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(("127.0.0.1", 12345))  # Server IP and port
+server_address = ("127.0.0.1", 12345)  # Server's IP and port
+client_socket.connect(server_address)
 
-print(client_socket.recv(1024).decode())  # Welcome message
+print(client_socket.recv(1024).decode())  # Receive welcome message from server
 
 while True:
     guess = input("Enter your guess: ")  # Player enters a guess
-    client_socket.send(guess.encode())  # Send guess to the server
-    response = client_socket.recv(1024).decode()  # Receive server response
+    client_socket.send(guess.encode())  # Send the guess to the server
+    response = client_socket.recv(1024).decode()  # Receive feedback from the server
     print(response)
     if "Congratulations" in response:
-        break
+        break  # Game ends when the player guesses correctly
 
 client_socket.close()
